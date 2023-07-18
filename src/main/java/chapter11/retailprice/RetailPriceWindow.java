02f009621314d4a04e5b98e598d2e20b17cfe142
Adam Golec <agolec@nexient.com>
2023-07-18 15:29:14 -0400

    update aspects of RetailPriceWindow to use a BorderLayout with different panels

--- src/main/java/chapter11/retailprice/RetailPriceWindow.java
+++ src/main/java/chapter11/retailprice/RetailPriceWindow.java
@@ -1,11 +1,11 @@
 package chapter11.retailprice;
 
 import javax.swing.*;
-import javax.swing.text.NumberFormatter;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
-import java.util.InputMismatchException;
 
 public class RetailPriceWindow extends JFrame {
     private JLabel wholesalePriceLabel;
@@ -14,8 +14,12 @@ public class RetailPriceWindow extends JFrame {
     private JTextField markupPercentTextField;
     private JButton calculateButton;
     private JButton someOtherButton;
+    private JButton exitButton;
 
     private JPanel panel;
+    private JPanel wholesalePricePanel;
+    private JPanel markupPercentPanel;
+    private JPanel operationsPanel;
 
     /**
      * Class Constructor.
@@ -30,7 +34,10 @@ public class RetailPriceWindow extends JFrame {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         buildPanel();
-        add(panel);
+        //add(panel);
+        add(wholesalePricePanel,BorderLayout.WEST);
+        add(markupPercentPanel, BorderLayout.EAST);
+        add(operationsPanel,BorderLayout.SOUTH);
         pack();
         setVisible(true);
     }
@@ -41,22 +48,32 @@ public class RetailPriceWindow extends JFrame {
         markupPercentLabel = new JLabel("Enter markup percent: ");
         markupPercentTextField = new JTextField(3);
         calculateButton = new JButton("Calculate");
-        someOtherButton = new JButton("LMAO YEET");
+        someOtherButton = new JButton("Nyahaha! I'm not the calculate button.");
+        exitButton = new JButton("Exit");
 
         //Add Action Listener to the button.
         calculateButton.addActionListener(new CalcButtonListener());
         someOtherButton.addActionListener(new CalcButtonListener());
+        exitButton.addActionListener(new ExitButtonListener());
 
 
         panel = new JPanel();
+        wholesalePricePanel = new JPanel();
+        markupPercentPanel = new JPanel();
+        operationsPanel = new JPanel();
+
+        setLayout(new BorderLayout());
 
         //add the label, text fields, buttons, to the panel.
-        panel.add(wholesalePriceLabel);
-        panel.add(wholesalePriceTextField);
-        panel.add(markupPercentLabel);
-        panel.add(markupPercentTextField);
-        panel.add(calculateButton);
-        panel.add(someOtherButton);
+        wholesalePricePanel.add(wholesalePriceLabel);
+        wholesalePricePanel.add(wholesalePriceTextField);
+        markupPercentPanel.add(markupPercentLabel);
+        markupPercentPanel.add(markupPercentTextField);
+        operationsPanel.add(calculateButton);
+        operationsPanel.add(someOtherButton);
+        operationsPanel.add(exitButton);
+
+
     }
     //Private inner class CalcButtonListener implements the ActionListener class.
     private class CalcButtonListener implements ActionListener{
@@ -79,12 +96,26 @@ public class RetailPriceWindow extends JFrame {
                 JOptionPane.showMessageDialog(null, "Retail price is : " + formatter.format(retailPrice));
             } catch(NumberFormatException er){
                 System.out.print("ERROR: Input for wholesale price or markup percent could not be parsed out.\n");
+                if(er.getMessage() == "empty String"){
+                    System.out.println("String was empty.");
+                }
                 System.out.print(er.getMessage() + "\n");
             }
         }
+
         private double calculateRetailPrice(double wholesalePrice, double markupPercent){
             return (wholesalePrice + wholesalePrice * (markupPercent / 100));
         }
-
+    }
+    private class ExitButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(null,"Exiting program");
+            System.exit(0);
+        }
+    }
+    public static void main(String[] args){
+        //Create an instance of the RetailPriceWindow class.
+        RetailPriceWindow window = new RetailPriceWindow();
     }
 }
