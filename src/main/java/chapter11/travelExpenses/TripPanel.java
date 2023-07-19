c9c87c97a9b5f9140740c152b3cc220f5b07b8fd
Adam <algolec1990@yahoo.com>
2023-07-19 16:23:39 -0400

    add TripPanel class to repo

--- src/main/java/chapter11/travelExpenses/TripPanel.java
+++ src/main/java/chapter11/travelExpenses/TripPanel.java
@@ -1,2 +1,161 @@
-package chapter11.travelExpenses;public class TripPanel {
+package chapter11.travelExpenses;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TripPanel extends JPanel {
+    private static final int ALLOWED_EXPENSES = 6000;
+
+    private JLabel dayLabel = new JLabel("Days on trip");
+    private JLabel airfareLabel = new JLabel("Airfare");
+    private JLabel carRentalLabel = new JLabel("Car Rental Fee");
+    private JLabel milesDrivenLabel = new JLabel("Miles Driven");
+    private JLabel parkingFeeLabel = new JLabel("Parking Fees");
+    private JLabel taxiChargesLabel = new JLabel("Taxi charges");
+    private JLabel conferenceLabel = new JLabel("Conference fee");
+    private JLabel lodgingLabel = new JLabel("Lodging charges");
+    private JTextField dayTF = new JTextField(10);
+    private JTextField airfareTF = new JTextField(10);
+    private JTextField carRentalTF = new JTextField(10);
+    private JTextField milesDrivenTF = new JTextField(10);
+    private JTextField parkingFeeTF = new JTextField(10);
+    private JTextField taxiChargesTF = new JTextField(10);
+    private JTextField conferenceTF = new JTextField(10);
+    private JTextField lodgingTF = new JTextField(10);
+    private double totalExpenses = 0.0;
+    TripPanel(){
+        buildPanel();
+    }
+    private void buildPanel(){
+        setLayout(new GridLayout(8,8));
+        setBackground(Color.decode("#FF3333"));
+        add(dayLabel);
+        add(dayTF);
+
+        add(airfareLabel);
+        add(airfareTF);
+
+        add(carRentalLabel);
+        add(carRentalTF);
+
+        add(milesDrivenLabel);
+        add(milesDrivenTF);
+
+        add(parkingFeeLabel);
+        add(parkingFeeTF);
+
+        add(lodgingLabel);
+        add(lodgingTF);
+
+        setStyle();
+    }
+    private void setStyle(){
+        dayLabel.setForeground(Color.WHITE);
+        airfareLabel.setForeground(Color.WHITE);
+        carRentalLabel.setForeground(Color.WHITE);
+
+        milesDrivenLabel.setForeground(Color.WHITE);
+        parkingFeeLabel.setForeground(Color.WHITE);
+        dayLabel.setForeground(Color.WHITE);
+
+        taxiChargesLabel.setForeground(Color.RED);
+        conferenceLabel.setForeground(Color.WHITE);
+        lodgingLabel.setForeground(Color.WHITE);
+
+        dayTF.setBackground(Color.RED);
+        dayTF.setForeground(Color.WHITE);
+
+        airfareTF.setBackground(Color.RED);
+        airfareTF.setForeground(Color.WHITE);
+
+        carRentalTF.setBackground(Color.RED);
+        carRentalTF.setForeground(Color.WHITE);
+
+        milesDrivenTF.setBackground(Color.RED);
+        milesDrivenTF.setForeground(Color.WHITE);
+
+        taxiChargesTF.setBackground(Color.RED);
+        taxiChargesTF.setForeground(Color.WHITE);
+
+        conferenceTF.setBackground(Color.RED);
+        conferenceTF.setForeground(Color.WHITE);
+
+        lodgingTF.setBackground(Color.RED);
+        lodgingTF.setForeground(Color.WHITE);
+        parkingFeeTF.setBackground(Color.RED);
+    }
+    private int getDays(){
+        int days = 0;
+
+        if(!dayTF.getText().isEmpty()){
+            days = Integer.parseInt(dayTF.getText());
+        }
+        return days;
+    }
+    private double getExpenses(){
+        final int DAILY_MEAL_EXPENSE_ALLOWANCE = 37;
+        final int DAILY_LODGING_EXPENSE_ALLOWANCE = 95;
+        final int DAILY_TAXI_EXPENSE_ALLOWANCE = 20;
+        final int DAILY_PARKING_EXPENSE_ALLOWANCE = 10;
+        final double CENTS_PER_MILE_TRAVELED = 0.27;
+        totalExpenses += (getDays() * DAILY_MEAL_EXPENSE_ALLOWANCE);
+
+        if(!airfareTF.getText().isEmpty()){
+            double airFare = Double.parseDouble(airfareTF.getText().toString());
+            totalExpenses += airFare;
+        }
+        if(!lodgingTF.getText().isEmpty()){
+            double lodging = Double.parseDouble(lodgingTF.getText().toString());
+            totalExpenses += (getDays() * DAILY_LODGING_EXPENSE_ALLOWANCE);
+        }
+        if(!conferenceTF.getText().isEmpty()){
+            double conferenceFees = Double.parseDouble(conferenceTF.getText());
+            totalExpenses += conferenceFees;
+        }
+        if(!taxiChargesTF.getText().isEmpty()){
+            totalExpenses += (getDays() * DAILY_TAXI_EXPENSE_ALLOWANCE);
+        }
+        if(!parkingFeeTF.getText().isEmpty()){
+            totalExpenses += (getDays() * DAILY_PARKING_EXPENSE_ALLOWANCE);
+        }
+        if(!milesDrivenTF.getText().isEmpty()){
+            double miles = Double.parseDouble(milesDrivenTF.getText());
+            if(miles > 0){
+                totalExpenses += miles * CENTS_PER_MILE_TRAVELED;
+            } else {
+                totalExpenses += 0;
+            }
+        }
+        if(!carRentalTF.getText().isEmpty()){
+            totalExpenses += Double.parseDouble(carRentalTF.getText());
+        }
+        return totalExpenses;
+    }
+    public double getTotalExpenses(){
+        return getExpenses();
+    }
+    public double getTotalAllowedExpenses(){
+        return ALLOWED_EXPENSES;
+    }
+    public double getExcess(){
+        double excess = 0;
+
+        if(totalExpenses > ALLOWED_EXPENSES){
+            excess = ALLOWED_EXPENSES - totalExpenses;
+        } else {
+            excess = 0;
+        }
+        return excess;
+    }
+
+    public double getSavedAmount(){
+        double savedAmount = 0;
+
+        if(totalExpenses < ALLOWED_EXPENSES){
+            savedAmount = ALLOWED_EXPENSES-totalExpenses;
+        } else {
+            savedAmount = 0;
+        }
+        return savedAmount;
+    }
 }
